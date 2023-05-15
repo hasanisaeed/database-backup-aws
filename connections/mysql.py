b81a5e9284e49d9810e2338a07740d472549ded9
Saeed <hassanisaeed19@yahoo.com>
2023-05-15 20:06:53 +0330

    feat: use `ini` config file

--- connections/mysql.py
+++ connections/mysql.py
@@ -1,13 +1,16 @@
+from typing import Union, Dict
+
 from connections.base import DBConnection
 
 
 class MySQLConnection(DBConnection):
-    def __init__(self, host: str, port: str, database: str, user: str, password: str):
-        self.host = host
-        self.port = port
-        self.database = database
-        self.user = user
-        self.password = password
+
+    def __init__(self, config: Dict[str, Union[str, int]]) -> None:
+        self.host: str = config['host']
+        self.port: int = config['port']
+        self.database: str = config['database']
+        self.user: str = config['user']
+        self.password: str = config['password']
 
     def connect(self):
         # Add your MySQL-specific connection logic here
