7ed0ca17f14ae98cb7177c3b8da6026cbfb28709
Saeed <hassanisaeed19@yahoo.com>
2023-05-15 19:59:17 +0330

    fix: add initializer and fix input parameters

--- backups/mysql.py
+++ backups/mysql.py
@@ -1,6 +1,10 @@
 from backups import DBBackup
+from connections.mysql import MySQLConnection
 
 
 class MySQLBackup(DBBackup):
-    def backup(self) -> None:
+    def __init__(self, connection: MySQLConnection):
+        self.connection = connection
+
+    def backup(self, backup_file_path: str, output_format: str) -> None:
         print("Backing up MySQL database...")
