4599750b9090c50cbc98e9cb4a156d2bd31bd1ee
Saeed <hassanisaeed19@yahoo.com>
2023-05-17 08:00:26 +0330

    fix: typo error in `bucket_name`

--- core/transmitters/hub/aws.py
+++ core/transmitters/hub/aws.py
@@ -9,7 +9,7 @@ class Boto3FileSender(FileSender):
     def __init__(self, config: Dict[str, str]) -> None:
         self.aws_access_key_id: str = config['aws_access_key_id']
         self.aws_secret_access_key: str = config['aws_secret_access_key']
-        self.aws_bucket_name: str = config['awa_bucket_name']
+        self.aws_bucket_name: str = config['aws_bucket_name']
         self.aws_destination_file_name: str = config['aws_destination_file_name']
 
     def send_file(self, file_path: str) -> None:
