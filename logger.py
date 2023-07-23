cb6a1330db482be22d0bf437b507afa8eb5a97e1
Saeed <hassanisaeed19@yahoo.com>
2023-07-23 17:44:13 +0330

    feat: setup logger

---
+++ logger.py
@@ -0,0 +1,46 @@
+import logging
+import os
+
+
+class LoggerSingleton:
+    _logger_instance = None
+    log_folder = 'logs'
+
+    @classmethod
+    def get_logger(cls):
+        if not cls._logger_instance:
+            cls._logger_instance = cls._create_logger()
+        return cls._logger_instance
+
+    @classmethod
+    def _create_logger(cls):
+        # create the logs folder if it doesn't exist
+        if not os.path.exists(cls.log_folder):
+            os.makedirs(cls.log_folder)
+
+        logger = logging.getLogger('logger')
+        logger.setLevel(logging.DEBUG)
+
+        # separate file handlers for each log level
+        info_file_handler = logging.FileHandler(os.path.join(cls.log_folder, 'info_log_file.log'))
+        error_file_handler = logging.FileHandler(os.path.join(cls.log_folder, 'error_log_file.log'))
+        debug_file_handler = logging.FileHandler(os.path.join(cls.log_folder, 'debug_log_file.log'))
+
+        # set the log levels for each file handler
+        info_file_handler.setLevel(logging.INFO)
+        error_file_handler.setLevel(logging.ERROR)
+        debug_file_handler.setLevel(logging.DEBUG)
+
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+        # set the formatter for each file handler
+        info_file_handler.setFormatter(formatter)
+        error_file_handler.setFormatter(formatter)
+        debug_file_handler.setFormatter(formatter)
+
+        # file handlers to the logger
+        logger.addHandler(info_file_handler)
+        logger.addHandler(error_file_handler)
+        logger.addHandler(debug_file_handler)
+
+        return logger
--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import argparse
+import os
 
 from datetime import datetime
 from subprocess import CalledProcessError
 import json
+import sched
+
+from logger import LoggerSingleton
 
 from core.connections.mysql import MySQLConnection
 from core.connections.postgres import PostgresConnection
 from core.backups.databases.mysql import MySQLBackup
 from core.backups.databases.postgres import PostgresBackup
-from core.exceptions.transmitters import SendingFileErrorException
 from core.transmitters.factory import FileSenderFactory
+
+logger = LoggerSingleton.get_logger()
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('--config-file', default='config.ini', required=False, help='Specify the config.ini file')
     parser.add_argument('--output-format', choices=['sql', 'gz'], required=True, help='Specify the output format')
+    parser.add_argument('--backup-folder', default='./backups', required=False, help='Default backup folder')
     parser.add_argument('--send-via', choices=['scp', 'boto3'], required=True, help='Specify the send method')
     parser.add_argument('--interval', default="12h", required=False, help='Interval')
     parser.add_argument('--remove-after-sending', default="true", required=False,
@@ -36,7 +42,7 @@ def get_connection(database, db_config):
     elif database == 'mysql':
         return MySQLConnection(db_config)
     else:
-        raise ValueError(f'Invalid database type: {database}')
+        logger.error(f'Invalid database type: {database}')
 
 
 def get_backup(database, connection):
@@ -45,7 +51,7 @@ def get_backup(database, connection):
     elif database == 'mysql':
         return MySQLBackup(connection)
     else:
-        raise ValueError(f'Invalid database type: {database}')
+        logger.error(f'Invalid database type: {database}')
 
 
 def perform_backup(backup, backup_file_path, output_format):
@@ -56,37 +62,40 @@ def send_backup(file_sender, backup_file_path):
     try:
         file_sender.send_file(backup_file_path)
     except CalledProcessError:
-        raise SendingFileErrorException(f"The file {backup_file_path} was not sent!")
+        logger.error(f'The file {backup_file_path} was not sent!')
 
 
 def remove_backup_file(backup_file_path):
     try:
         import os
         os.remove(backup_file_path)
-        print(f">> Temp file '{backup_file_path}' deleted successfully.")
+        logger.info(f">> Temp file '{backup_file_path}' deleted successfully.")
     except OSError as e:
-        print(f">> Error deleting file '{backup_file_path}': {e}")
+        logger.info(f">> Error deleting file '{backup_file_path}': {e}")
 
 
 def main():
     args = parse_arguments()
     config = read_config(args.config_file)
-    sender_type = args.send_via
     databases = config['databases']
 
     for db_config in databases:
+        database_type = db_config.get('type')
+        connection = get_connection(database_type, db_config)
 
-        connection = get_connection(db_config.get('type'), db_config)
-
-        backup = get_backup(args.database, connection)
+        backup = get_backup(database_type, connection)
 
         output_format = args.output_format
-        backup_file_path = f"./backup_{datetime.now():%a_%Y_%m_%d_%H%M%S}.{output_format}"
+
+        backup_folder = args.backup_folder
+
+        if not os.path.exists(backup_folder):
+            os.makedirs(backup_folder)
+        backup_file_path = os.path.join(backup_folder, f"backup_{datetime.now():%a_%Y_%m_%d_%H%M%S}.{output_format}")
 
         perform_backup(backup, backup_file_path, output_format)
 
-        if sender_type not in config.get('transmitters'):
-            raise ValueError(f'{sender_type} section not found in config.')
+        sender_type = args.send_via
 
         sender_config = config.get('transmitters')[sender_type]
         file_sender = FileSenderFactory.create_file_sender(sender_type, sender_config)
@@ -99,5 +108,13 @@ def main():
             remove_backup_file(backup_file_path)
 
 
+def run_scheduler():
+    import time
+    s = sched.scheduler(time.time, time.sleep)
+    while True:
+        s.enter(5, 1, main, ())
+        s.run()
+
+
 if __name__ == '__main__':
-    main()
+    run_scheduler()
